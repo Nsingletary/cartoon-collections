8c00fe73531f590fae8926df1f4d9976a43afbbe
Nsingletary <singletarybush@gmail.com>
2020-11-20 21:29:11 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,11 +11,11 @@ def summon_captain_planet(planeteers)
   end
 end
 
-def long_planeteer_calls(strings)
+def long_planeteer_calls(calls)
   calls.any? {|call| call.to_s.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.detect do |string|
