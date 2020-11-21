c359590a154297d2f612b8e15872822417f8e0f3
Nsingletary <singletarybush@gmail.com>
2020-11-20 21:26:55 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(dwarves)
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.map do |planeteer|
+    planeteer.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
